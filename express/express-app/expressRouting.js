26058354e855cf7747c169303600056c0c308c33
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-09-06 15:20:51 +0530

    rendering ejs file

--- express/express-app/expressRouting.js
+++ express/express-app/expressRouting.js
@@ -3,8 +3,11 @@
 const express = require('express');
 const app = express();
 
+app.set("view engine", 'ejs')
+
+
 app.get('/', (req, res)=>{
-    res.send('Hello World');
+    res.render('index');
 })
 app.get('/about', (req, res)=>{
     res.send('This is about page');
