ebdf498368a2ff24f7e3e9df5f1671c13474074e
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-09-07 20:33:21 +0530

    created middleware to add static files

--- express/form-validation/form.js
+++ express/form-validation/form.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(morgan('dev'));
 
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
 
 
 app.set('view engine', 'ejs');
