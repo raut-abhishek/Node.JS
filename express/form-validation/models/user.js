ec823e92d553c90f6694387fcbc9109770365f0d
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-09-08 18:33:24 +0530

    defined users schema

--- express/form-validation/models/user.js
+++ express/form-validation/models/user.js
@@ -1,1 +1,12 @@
- 
+// schema for the user database
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username : String,
+    email : String,
+    password : String
+})  
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
